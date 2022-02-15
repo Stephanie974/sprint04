d1ac6a0d0a175a8fab556bbb97d0e420e6654c70
Stephanie974 <sturpin677@gmail.com>
2022-02-15 15:39:31 +0400

    encore commit00

---
+++ day_01/starter_day01_L00/datatypes.js
@@ -0,0 +1,24 @@
+let prenom2 = "Stephanie";
+console.log(typeof prenom2);
+let age3 = 29;
+console.log(typeof age3);
+let place = "Simplon";
+console.log(typeof place);
+let voiture = "audi";
+
+let firstname = " Julie1";
+let name2 = " Louis1";
+let etat = "matrimonial1";
+let city = "Paris1";
+let age4 = "301";
+
+let firstname1 = " Julie",
+  name1 = " Louis",
+  etat1 = "matrimonial",
+  city1 = "Paris",
+  age5 = "30";
+
+let myAge = "26";
+console.log("J'ai " + myAge + " ans.");
+let yourAge = "30";
+console.log("Vous avez " + yourAge + " ans.");
