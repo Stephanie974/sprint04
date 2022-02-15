35956149cc26001f9aab086bf953129590f6bfc1
Stephanie974 <sturpin677@gmail.com>
2022-02-15 15:38:53 +0400

    suite00

--- day_01/starter_day01_L00/variable.js
+++ day_01/starter_day01_L00/variable.js
@@ -1,18 +1,46 @@
 // *****Exercices: Data Types --> Level00 [4]*****
 
-
 // Declaring different variables of different data types
+// déclarez des variables et attribuez des types de données chaîne, boolean, undefined et null
 
+var work = "developer";
+console.log("work");
 
+let name = null;
+console.log("name=", name);
 
+let z = true;
+console.log("z=", z);
+
+let age = undefined;
+console.log("age=", age);
 
 // Declaring variables with number values
 
-
-
-
-
-
 // Variables can also be declaring in one line separated by comma
 
+let user = "Stephanie",
+  age1 = 29,
+  address = "Reunion";
+console.log(
+  "Bonjour, je suis " +
+    user +
+    ", j'ai " +
+    age1 +
+    " ans et j'habite à la " +
+    address +
+    "."
+);
 
+var user2 = "Yannick",
+  age2 = 20,
+  address2 = "Maurice";
+console.log(
+  "Hello, je m'appelle " +
+    user2 +
+    ", j'ai " +
+    age2 +
+    " ans et j'habite à l'ile " +
+    address2 +
+    "."
+);
