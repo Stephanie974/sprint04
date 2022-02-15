77adae41517662fc4db8ac2587362428bfbe1fe5
Stephanie974 <sturpin677@gmail.com>
2022-02-15 15:37:00 +0400

    commit 00

--- day_01/starter_day01_L00/comments.js
+++ day_01/starter_day01_L00/comments.js
@@ -1,1 +1,9 @@
 // Exercices: Data Types --> Level00 [1,2,3]
+
+// les commmentaires peuvent rendre le code lisible
+
+// Bienvenue dans JavaScript
+
+// les commentaires peuvent rendre
+// le code lisible, facile à
+// réutiliser et informatif
