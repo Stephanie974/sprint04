78ab8b5c20e952b85af167a549a7d6589b2d4f87
Stephanie974 <sturpin677@gmail.com>
2022-02-18 15:15:34 +0400

    sprint_01

---
+++ day_01/starter_day01_boolean_L01/script.js
@@ -0,0 +1,97 @@
+// const date = {
+//   firstName: "Turpin",
+//   lastName: "Stephanie",
+//   country: "Reunion",
+//   city: "Saint Denis",
+//   age: 29,
+//   isMarried: "celibataire",
+// };
+// console.log(typeof "Turpin");
+// console.log(typeof "Stephanie");
+// console.log(typeof "Reunion");
+// console.log(typeof "Saint Denis");
+// console.log(typeof 29);
+// console.log(typeof "celibataire");
+
+// let num = 10;
+// let str = "10";
+// console.log(num == str);
+
+// let num1 = 10;
+// let num2 = parseInt("9,8");
+// console.log(num1 == num2);
+
+// var btrue = 9 > 7;
+// var btrue0 = 6 > 3;
+// var btrue1 = 4 > 2;
+// var bfalse = 1 > 2;
+// var bfalse0 = 2 > 4;
+// var bfalse1 = 3 > 5;
+// console.log(btrue);
+// console.log(btrue0);
+// console.log(btrue1);
+// console.log(bfalse);
+// console.log(bfalse0);
+// console.log(bfalse1);
+
+// console.log(4 > 3);
+// // vrai
+// console.log(4 >= 3);
+// // faux
+// console.log(4 < 3);
+// // faux
+// console.log(4 <= 3);
+// // vrai
+// console.log(4 == 4);
+// // vrai
+// console.log(4 === 4);
+// // vrai
+// console.log(4 != 4);
+// // faux
+// console.log(4 !== 4);
+// // faux
+// console.log(4 != "4");
+// // faux
+// console.log(4 == "4");
+// // vrai
+// console.log(4 === "4");
+// // vrai
+
+// // Trouvez la longueur 'length' de python et du jargon et faites une fausse déclaration de comparaison.
+// var longueur = "python".length;
+// console.log(longueur);
+// var longueur0 = " jargon".length;
+// console.log(longueur);
+// console.log(longueur > longueur0);
+
+// console.log(4 > 3 && 10 < 12);
+// // false
+// console.log(4 > 3 && 10 > 12);
+// // true
+// console.log(4 > 3 || 10 < 12);
+// // true
+// console.log(4 > 3 || 10 > 12);
+// // true
+// console.log(!(4 > 3));
+// // false
+// console.log(!(4 < 3));
+// // true
+// console.log(!false);
+// // true
+// console.log(!(4 > 3 && 10 < 12));
+// // false
+// console.log(!(4 > 3 && 10 > 12));
+// // true
+// console.log(!(4 === "4"));
+// // true
+
+// let include = "dragon, python";
+// console.log(include.includes("on"));
+
+let jour = new Date();
+console.log(jour.getFullYear());
+console.log(jour.getMonth() + 1);
+console.log(jour.getDay());
+console.log(jour.getHours());
+
+console.log(jour.getTime());
